2b1397acbf5c91a9a4230ee2109087fa2e5bdc8d
takuhito-h <bluesbug@gmail.com>
2016-06-15 19:54:32 +0900

    first commit

---
+++ gulp/tasks/css.js
@@ -0,0 +1,28 @@
+/*------------------------------------------------------------------------------------------------
+
+    css
+
+------------------------------------------------------------------------------------------------*/
+var gulp       = require('gulp');
+var gutil      = require('gulp-util');
+var sass       = require('gulp-sass');
+var cssnext    = require('gulp-cssnext');
+var sourcemaps = require('gulp-sourcemaps');
+var config     = require('../config').css;
+
+var isRelease = gutil.env.release ? gutil.env.release : false;
+
+/*------------------------------------------------------------------
+    task
+------------------------------------------------------------------*/
+gulp.task('css', function() {
+
+    return gulp.src(config.src)
+        .pipe(sourcemaps.init())
+            .pipe(sass(config.sass).on('error', sass.logError))
+            .pipe(cssnext(config.cssnext))
+        .pipe(isRelease ? gutil.noop() : sourcemaps.write('./'))
+            .pipe(gulp.dest(config.dest))
+    ;
+
+});
